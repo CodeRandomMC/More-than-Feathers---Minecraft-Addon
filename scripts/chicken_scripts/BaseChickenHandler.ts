869185fbabd38710730e44663b1cce51a587e210
CodeRandomMC <86018781+CodeRandomMC@users.noreply.github.com>
2025-06-03 10:34:02 +0100

    Fix chicken interaction logic: update entity type check to use resource chicken now done tests on normal chicken and enhance saturation logging for better debugging.

--- scripts/chicken_scripts/BaseChickenHandler.ts
+++ scripts/chicken_scripts/BaseChickenHandler.ts
@@ -1,4 +1,5 @@
 import { ItemStack, PlayerInteractWithEntityAfterEvent, world } from "@minecraft/server";
+import { Logger } from "../utils/CRSLogger";
 
 export class BaseChickenHandler {
   constructor() {
@@ -11,7 +12,7 @@ export class BaseChickenHandler {
       const entity = event.target;
 
       // Check if the entity is a chicken
-      if (entity.typeId !== "minecraft:chicken") return;
+      if (entity.typeId !== "crs_mf:resource_chicken") return;
 
       // Check if chcicken has the is_baby component
       const isBaby = entity.getComponent("minecraft:is_baby");
@@ -23,25 +24,38 @@ export class BaseChickenHandler {
 
       // Check if saturation is below 80 before feeding
       let saturation = (entity.getDynamicProperty("crs_mf:saturation") as number) || 0;
+      if (Logger.debugEnabled) {
+        Logger.debug(`Chicken saturation before feeding: ${saturation}`);
+      }
       if (saturation >= 80) {
+        if (Logger.debugEnabled) {
+          Logger.debug(`Feeding prevented: Chicken saturation (${saturation}) is 80 or higher.`);
+        }
         return;
       }
 
       if (event.itemStack && event.itemStack.typeId === "crs_mf:chicken_feed") {
+        if (Logger.debugEnabled) {
+          Logger.debug(`Player ${player.name} fed a chicken with chicken feed.`);
+        }
         event.itemStack.amount--;
-        const feedIndex = event.player
+        const feedIndex = player
           .getComponent("minecraft:inventory")
           ?.container.find(new ItemStack("crs_mf:chicken_feed"));
 
         if (feedIndex !== undefined && feedIndex !== -1) {
-          event.player
+          player
             .getComponent("minecraft:inventory")
             ?.container.setItem(feedIndex, new ItemStack("crs_mf:chicken_feed", event.itemStack.amount));
         }
 
         // Increase the chickens saturation property
+        const oldSaturation = saturation;
         saturation = Math.min(100, Math.max(0, saturation + 10));
         entity.setDynamicProperty("crs_mf:saturation", saturation);
+        if (Logger.debugEnabled) {
+          Logger.debug(`Chicken saturation changed from ${oldSaturation} to ${saturation}`);
+        }
       }
     });
   }
